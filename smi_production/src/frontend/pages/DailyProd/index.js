e14e8c68becc908c41ea1ed2573bd6f61de76e2b
BenPerraud <benjaminperraud@outlook.fr>
2023-07-11 11:30:47 +0200

    v18

--- smi_production/src/frontend/pages/DailyProd/index.js
+++ smi_production/src/frontend/pages/DailyProd/index.js
@@ -58,7 +58,7 @@ function DailyProd () {
     
     useEffect(() => {
         const reqDate = parseInt(dateURL)
-        fetch("http://192.168.74.1:3000/api/production/date/"+dateURL)
+        fetch("http://localhost:3000/api/production/date/"+dateURL)
             .then(res => res.json())
             .then(res => setProductions(createNewObjectProd(createDayProd(res, reqDate))))
             .catch(error => alert("Erreur : " + error))
