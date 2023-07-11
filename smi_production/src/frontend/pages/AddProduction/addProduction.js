39b2153dc726b6222d572336e1616ab233f513ed
BenPerraud <benjaminperraud@outlook.fr>
2023-07-11 17:43:25 +0200

    g

--- smi_production/src/frontend/pages/AddProduction/addProduction.js
+++ smi_production/src/frontend/pages/AddProduction/addProduction.js
@@ -45,9 +45,8 @@ function AddProduction ({pi, client, designation, setCount}) {
         formData.append("date", date)
         formData.append("prodTime", totTimeProd)
         formData.append("operator", operator)
-        console.log(formData.quantityProd)
         
-        if (h === 0 || m === 0 || formData.quantityProd === "" || formData.quantityWaste === "") {
+        if (h === 0 || m === 0 || formData.get("quantityProd") === "" || formData.get("quantityWaste") === "" || formData.get("operator") === "") {
             alert("Un des champs du questionnaire n'est pas rempli, veuillez recommencer")
         } else { 
             if( operator.length > new Set(operator).size) {
