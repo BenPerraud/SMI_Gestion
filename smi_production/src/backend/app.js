6f53540914d6def0a0e8891d089359f85422fe1a
BenPerraud <benjaminperraud@outlook.fr>
2023-07-11 10:16:13 +0200

    v_cors4

--- smi_production/src/backend/app.js
+++ smi_production/src/backend/app.js
@@ -47,7 +47,7 @@ const productionRouter = require("./routes/production")
 
 app.use(express.json())
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*')
+  res.setHeader('Access-Control-Allow-Origin', 'http://192.168.74.1:3000')
   res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Content-Length, Authorization')
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS')
   next()
