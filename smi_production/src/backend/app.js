73abde7f423ad3e082acfa5dae8566487a2f97a3
BenPerraud <benjaminperraud@outlook.fr>
2023-07-11 15:22:01 +0200

    v65

--- smi_production/src/backend/app.js
+++ smi_production/src/backend/app.js
@@ -1,7 +1,6 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const { MongoClient, ServerApiVersion } = require('mongodb')
-const { expressCspHeader, INLINE, NONE, SELF } = require('express-csp-header')
 require("dotenv").config()
 
 
