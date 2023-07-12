512afefcc2be54d36c920927115e39291ce3c7a4
BenPerraud <benjaminperraud@outlook.fr>
2023-07-12 12:22:11 +0200

    v115

--- smi_production/ecosystem.config.js
+++ smi_production/ecosystem.config.js
@@ -1,12 +1,13 @@
 module.exports = {
-    apps : [{
-      name   : "frontend",
-      script : "./package.json",
-      args   : "limit"
-    },{
-      name   : "backend",
-      script : "./src/backend/package.json",
-      args   : "rotate"
-    }]
+    apps : [
+        {
+            name   : "backend",
+            script : "./src/backend/package.json",
+          },
+          {
+            name   : "frontend",
+            script : "./package.json",
+            }
+    ]
   }
      
