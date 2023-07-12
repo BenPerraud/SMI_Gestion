15f5038530da7cdf8d44f4c714d79a66bb87299f
BenPerraud <benjaminperraud@outlook.fr>
2023-07-12 16:35:31 +0200

    vdef

--- smi_production/src/frontend/header/index.js
+++ smi_production/src/frontend/header/index.js
@@ -15,7 +15,7 @@ function Header () {
                 <NavLink to={todayURL} className={currentLocation.pathname === todayURL ? "navBar_link_element_current" : "navBar_link_element" }>Production à la journée</NavLink>
                 <NavLink to="/Analyse-par-Pi" className={currentLocation.pathname === "/Analyse-par-Pi" ? "navBar_link_element_current" : "navBar_link_element" }>Analyse par PI</NavLink>
                 <NavLink to="/Analyse-globale" className={currentLocation.pathname === "/Analyse-globale" ? "navBar_link_element_current" : "navBar_link_element" }>Analyse globale</NavLink>
-                <NavLink to="/Ajouter-un-Pi" className={currentLocation.pathname === "/Ajouter-un-Pi" ? "navBar_link_element_current" : "navBar_link_element" }>Ajouter un Pi</NavLink>
+                <NavLink to="/Ajouter-un-Pi" className={currentLocation.pathname === "/Ajouter-un-Pi" ? "navBar_link_element_current" : "navBar_link_element" }>Ajouter un PI</NavLink>
             </div>
         </div>
     )
