da4f53bbc0a5b9d7ff7581430e9a7b39e744c0ed
BenPerraud <benjaminperraud@outlook.fr>
2023-07-13 11:39:15 +0200

    v1

--- smi_production/src/frontend/components/routesApi.js
+++ smi_production/src/frontend/components/routesApi.js
@@ -1,2 +1,4 @@
-export const productionAPI = "http://192.168.74.1:3001/api/production"
-export const operatorAPI = "http://192.168.74.1:3001/api/operator"
+const productionAPI = "http://192.168.74.1:3001/api/production"
+const operatorAPI = "http://192.168.74.1:3001/api/operator"
+
+export default (productionAPI, operatorAPI)
